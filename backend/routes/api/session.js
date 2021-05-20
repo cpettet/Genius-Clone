50aef7d377c3f142b8d51b13313c43cf209dfa7c
cpettet <cpettet11@gmail.com>
2021-05-20 08:55:33 -0700

    user restore implemented

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -34,4 +34,16 @@ router.delete("/", (_req, res) => {
   return res.json({ message: "success" });
 });
 
+// Restore session user
+router.get("/", restoreUser, (req, res) => {
+    const { user } = req;
+    if (user) {
+        return res.json({
+            user: user.toSafeObject()
+        });
+    } else {
+        return res.json({});
+    }
+});
+
 module.exports = router;
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 
-const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { setTokenCookie, requireAuth, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
 const router = express.Router();
