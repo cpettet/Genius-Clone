02cb3ab8c6c9fc77ea14033a28ecae8c4c6a4f5e
cpettet <cpettet11@gmail.com>
2021-08-06 00:58:08 -0700

    Algorithm that works in o(n) for 'rendering' the current spans seems to work (rendering meaning it separates characters in an annotation by capitalizing them). Getting late, going to sleep and check before building out the JSX elements tomorrow.

--- frontend/src/components/LyricsShow/LyricsShow.js
+++ frontend/src/components/LyricsShow/LyricsShow.js
@@ -1,14 +1,59 @@
 import { useState } from "react";
 import styles from "./LyricsShow.module.css";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 const LyricsShow = ({ editMode, setAnnotateBox, setAnnotateMode, track }) => {
   const dispatch = useDispatch();
-  const startIndices = Object.values(track?.Annotations).sort(
+
+  // sorts the annotations in order of start index
+  const sortedAnnotations = Object.values(track?.Annotations).sort(
     (a, b) => a.startIndex - b.startIndex
   );
 
-  console.log("Start indices", startIndices);
+  const sortedStartingIndices = sortedAnnotations.map(
+    (annotation) => annotation.startIndex
+  );
+
+  const allSortedIndices = sortedAnnotations.reduce((acc, next) => {
+    return acc.concat([next.startIndex, next.endIndex]);
+  }, []);
+
+  console.log("All indices in order:", allSortedIndices);
+
+  const renderedSpans = [];
+  let pairCount = 0;
+  let currentSpan = [];
+  let inCurrentPair = false;
+  for (let i = 0; i < track.lyrics.length; i++) {
+    const character = track.lyrics[i];
+    const firstToCompare = allSortedIndices[pairCount] && allSortedIndices[pairCount][0];
+    const secondToCompare = allSortedIndices[pairCount] && allSortedIndices[pairCount][1];
+    if (firstToCompare === i) {
+      // entering the start of an annotation
+      renderedSpans.push([currentSpan.join("").toLowerCase()]);
+      inCurrentPair = true;
+      currentSpan = [character];
+    } else if (secondToCompare === i) {
+      // ending an annotation
+      renderedSpans.push([currentSpan.join("").toUpperCase()]);
+      inCurrentPair = false;
+      currentSpan++;
+      currentSpan = [character];
+    } else if (inCurrentPair) {
+      currentSpan.push(character);
+    } else if (track.lyrics.length - 1 === i && inCurrentPair) {
+      // last element, in an annotation
+      currentSpan.push(character);
+      renderedSpans.push([currentSpan.join("").toUpperCase()]);
+    } else if (track.lyrics.length - 1 === i && !inCurrentPair) {
+      // last element, not in an annotation
+      currentSpan.push(character);
+      renderedSpans.push([currentSpan.join("").toLowerCase()]);
+    } else {
+      currentSpan.push(character);
+    }
+  }
+  console.log("Rendered spans:", renderedSpans)
 
   return (
     <p
