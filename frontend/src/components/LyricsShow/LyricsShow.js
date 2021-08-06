d1961a7bfcd87ed22729dc8e9d7ad6367d0c4f28
cpettet <cpettet11@gmail.com>
2021-08-06 09:33:36 -0700

    Renders annotated parts of lyrics

--- frontend/src/components/LyricsShow/LyricsShow.js
+++ frontend/src/components/LyricsShow/LyricsShow.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import styles from "./LyricsShow.module.css";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import React from "react";
 
 const LyricsShow = ({ editMode, setAnnotateBox, setAnnotateMode, track }) => {
   const dispatch = useDispatch();
@@ -15,10 +17,25 @@ const LyricsShow = ({ editMode, setAnnotateBox, setAnnotateMode, track }) => {
   );
 
   const allSortedIndices = sortedAnnotations.reduce((acc, next) => {
-    return acc.concat([next.startIndex, next.endIndex]);
+    return [...acc, [next.startIndex, next.endIndex]];
   }, []);
 
   console.log("All indices in order:", allSortedIndices);
+
+  const buildUnannotatedSpan = (characterArray) => {
+    const spanOfChars = characterArray.join("");
+
+    return <span className={styles.lyrics__unannotated}>{spanOfChars}</span>;
+  };
+
+  const buildAnnotatedSpan = (characterArray, pairCount) => {
+    const spanOfChars = characterArray.join("");
+    return (
+      <Link to={`/tracks/${track.id}/#${sortedAnnotations[pairCount].id}`}>
+        <span className={styles.lyrics__annotated}>{spanOfChars}</span>
+      </Link>
+    );
+  };
 
   const renderedSpans = [];
   let pairCount = 0;
@@ -26,46 +43,65 @@ const LyricsShow = ({ editMode, setAnnotateBox, setAnnotateMode, track }) => {
   let inCurrentPair = false;
   for (let i = 0; i < track.lyrics.length; i++) {
     const character = track.lyrics[i];
-    const firstToCompare = allSortedIndices[pairCount] && allSortedIndices[pairCount][0];
-    const secondToCompare = allSortedIndices[pairCount] && allSortedIndices[pairCount][1];
+    const firstToCompare =
+      allSortedIndices[pairCount] && allSortedIndices[pairCount][0];
+    const secondToCompare =
+      allSortedIndices[pairCount] && allSortedIndices[pairCount][1];
+    console.log(
+      "Character:",
+      character,
+      "\nFirst Index:",
+      firstToCompare,
+      "\nSecond Index:",
+      secondToCompare
+    );
     if (firstToCompare === i) {
       // entering the start of an annotation
-      renderedSpans.push([currentSpan.join("").toLowerCase()]);
+      renderedSpans.push(buildUnannotatedSpan(currentSpan));
       inCurrentPair = true;
       currentSpan = [character];
     } else if (secondToCompare === i) {
       // ending an annotation
-      renderedSpans.push([currentSpan.join("").toUpperCase()]);
+      renderedSpans.push(buildAnnotatedSpan(currentSpan, pairCount));
       inCurrentPair = false;
-      currentSpan++;
+      pairCount++;
       currentSpan = [character];
     } else if (inCurrentPair) {
+      // characters in annotation
       currentSpan.push(character);
     } else if (track.lyrics.length - 1 === i && inCurrentPair) {
       // last element, in an annotation
       currentSpan.push(character);
-      renderedSpans.push([currentSpan.join("").toUpperCase()]);
+      renderedSpans.push(buildAnnotatedSpan(currentSpan, pairCount));
     } else if (track.lyrics.length - 1 === i && !inCurrentPair) {
       // last element, not in an annotation
       currentSpan.push(character);
-      renderedSpans.push([currentSpan.join("").toLowerCase()]);
+      renderedSpans.push(buildUnannotatedSpan(currentSpan));
     } else {
+      // characters not in annotation
       currentSpan.push(character);
     }
   }
-  console.log("Rendered spans:", renderedSpans)
+  console.log("Rendered spans:", renderedSpans);
 
   return (
-    <p
-      hidden={editMode}
-      onMouseDown={() => {
-        setAnnotateMode(false);
-        setAnnotateBox(false);
-      }}
-      className={styles.track__lyrics}
-    >
-      {track.lyrics}
-    </p>
+    <>
+      <h3>Here's where the rendered spans start</h3>
+      <p
+        className={styles.track__lyrics}
+      >{React.Children.toArray(renderedSpans)}</p>
+      <h3>Here's where the lyrics start</h3>
+      <p
+        hidden={editMode}
+        onMouseDown={() => {
+          setAnnotateMode(false);
+          setAnnotateBox(false);
+        }}
+        className={styles.track__lyrics}
+      >
+        {track.lyrics}
+      </p>
+    </>
   );
 };
 
